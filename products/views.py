8f36c4c87cf4881920543ea3ead9e46b17893447
Delboy <Derek.degoey@hotmail.co.uk>
2022-04-26 15:21:55 +0000

    add product cards to products template

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -8,9 +8,11 @@ def all_products(request):
     """ A view to show all products """
 
     products = Product.objects.all()
+    categories = Category.objects.all()
 
     context = {
         'products': products,
+        'categories': categories,
     }
 
     return render(request, 'products/products.html', context)
