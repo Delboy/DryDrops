95f843d58048892acd7002a4c7f6c650ed666b87
Delboy <Derek.degoey@hotmail.co.uk>
2022-05-03 17:08:07 +0000

    added checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,6 +25,7 @@ class OrderForm(forms.ModelForm):
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County, State or Province',
+            'country': 'Country',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
