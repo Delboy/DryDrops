9b0ef9a193e39452a3075680f69d4a35b58b77e2
Delboy <Derek.degoey@hotmail.co.uk>
2022-05-04 15:55:27 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KmwngAVIL77aOAXS7GXfc3CIpJanaQbTWbUbbal2LuRB2IwqgFufj7aijuTdFyryYSY7jpATP6FB6LzsBBiBRxm00aIG2J6NO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
