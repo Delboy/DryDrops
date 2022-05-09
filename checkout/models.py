9404e5df0095bef79e296936910cb63b3ae4530b
Delboy <Derek.degoey@hotmail.co.uk>
2022-05-09 14:09:37 +0000

    Updated countries field on checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -1,11 +1,10 @@
-from django.db import models
-
-# Create your models here.
-
 import uuid
 
+from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
+from django_countries.fields import CountryField
 
 from products.models import Product
 
@@ -20,7 +19,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country', null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0
