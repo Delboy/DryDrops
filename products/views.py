557035e390f54a7f4bc0f26ba05ed96ac9fb582c
Delboy <Derek.degoey@hotmail.co.uk>
2022-05-12 10:51:37 +0000

    Finished edit product functionality, added delete product functionality

--- products/views.py
+++ products/views.py
@@ -78,7 +78,7 @@ def product_detail(request, product_id):
         liked = False
         if product.likes.filter(id=request.user.id).exists():
             liked = True
-        
+
         context = {
             'product': product,
             'liked': liked,
@@ -92,9 +92,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -106,6 +106,7 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
 
 def edit_product(request, product_id):
     """ Edit a product in the store """
@@ -129,3 +130,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
