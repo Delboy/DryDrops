5c0c312611fe954e3438d0016ec0f5fe71454019
Delboy <Derek.degoey@hotmail.co.uk>
2022-05-13 10:21:54 +0000

    updated image field in product form

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -6,7 +7,14 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ('category', 'sku', 'name', 'description', 'price', 'image', 'featured') 
+        fields = (
+            'category', 'sku', 'name', 'description',
+            'price', 'featured', 'image',
+            )
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
