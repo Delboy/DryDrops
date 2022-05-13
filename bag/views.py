d13a6e9c74996260fd34c61068cde606c4621e00
Delboy <Derek.degoey@hotmail.co.uk>
2022-05-13 13:22:23 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -12,6 +14,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ A view that handles adding items to the bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -20,7 +23,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-
+    messages.success(request, f'Added {product.name} to your bag')
     request.session['bag'] = bag
     return redirect(redirect_url)
 
@@ -28,24 +31,26 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust item's quantity in bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
-
+        messages.success(request, f'Removed {product.name} from your bag')
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
     """ Removes item from bag """
-    
+    product = get_object_or_404(Product, pk=item_id)
     bag = request.session.get('bag', {})
     bag.pop(item_id)
-
+    messages.success(request, f'Removed {product.name} from your bag')
     request.session['bag'] = bag
     return HttpResponse(status=200)
    
