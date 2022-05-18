ba1a3631737dd750c7bbc78a2f739ce86c5ab48f
Delboy <Derek.degoey@hotmail.co.uk>
2022-05-18 13:46:15 +0000

    Updated review admin

--- review/admin.py
+++ review/admin.py
@@ -2,17 +2,7 @@ from django.contrib import admin
 from .models import Review
 
 
-admin.site.register(Review)
-
-
 class ReviewAdmin(admin.ModelAdmin):
-    """
-    Admin settings to display list of Reviews
-    Ordered by date created and ratings
-    filter by ratings
-    """
-
-    model = Review
     list_display = (
         'product',
         'created_on',
@@ -21,5 +11,8 @@ class ReviewAdmin(admin.ModelAdmin):
         'body',
     )
 
-    ordering = ('-created_on', '-rating', )
-    list_filter = ('rating',)
+    ordering = ('-created_on', 'rating',)
+    list_filter = ('product', 'rating',)
+
+
+admin.site.register(Review, ReviewAdmin)
