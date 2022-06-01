47a27aed90d1711e076d0915b67b003657e75bd5
Delboy <Derek.degoey@hotmail.co.uk>
2022-06-01 11:14:22 +0000

    Added top rated section to index page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from products.models import Product
+
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    top_products = Product.objects.all().order_by('-rating')
+    context = {
+        'top_products': top_products,
+    }
+    return render(request, 'home/index.html', context)
