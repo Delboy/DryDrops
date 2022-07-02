c248e1f16836901946c3d71813cae4d4ed065872
Delboy <Derek.degoey@hotmail.co.uk>
2022-07-02 15:50:42 +0000

    Updated coupon section on bag page

--- coupons/forms.py
+++ coupons/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from coupons.models import Coupon
 
 
 class CouponApplyForm(forms.Form):
     code = forms.CharField()
+
+    class Meta:
+        model = Coupon
+        fields = ['code']
+  
+    def __init__(self, *args, **kwargs):
+        super(CouponApplyForm, self).__init__(*args, **kwargs)
+        self.fields['code'].label = ""
