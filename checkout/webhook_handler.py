9b06ec0947735c0134c887178c07f780c98c2668
Delboy <Derek.degoey@hotmail.co.uk>
2022-07-03 13:41:21 +0000

    Correcting mistake in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -23,8 +23,7 @@ class StripeWH_Handler:
         discount = order.order_subtotal - order.order_total
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'order': order,
-            'discount': discount,})
+            {'order': order, 'discount': discount})
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
