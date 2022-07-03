246020fa2f1ac9ef7242d48bc35ecc71a3dbe919
Delboy <Derek.degoey@hotmail.co.uk>
2022-07-03 13:08:36 +0000

    Adjusted checkout success summary section

--- checkout/views.py
+++ checkout/views.py
@@ -150,6 +150,7 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    discount = order.order_subtotal - order.order_total
 
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
@@ -182,6 +183,7 @@ def checkout_success(request, order_number):
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
+        'discount': discount,
     }
 
     return render(request, template, context)
